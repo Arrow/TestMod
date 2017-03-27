80b546825cb09372ecaa32e3971104b401d0edd1
David <bloggingarrow@gmail.com>
2017-03-27 11:40:01 -0500

    Initial Commit

---
+++ src/main/java/com/somniastellarum/testmod/TestMod.java
@@ -0,0 +1,31 @@
+package com.somniastellarum.testmod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="testmod", name="Test Mod", version="1.11.2-1.0")
+public class TestMod
+{
+    @Mod.Instance("TestMod")
+    public static TestMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
